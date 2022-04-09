c7cce126b43b4ebd852fe4631053e628dd01ecc5
Messi_G <negash4@gmail.com>
2022-04-09 12:19:28 -0500

    Order class

---
+++ Stark-MunderDifflin/Models/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace Stark_MunderDifflin.Models
+{
+    public class Order
+    {
+       public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int customerId { get; set; }
+    }
+}
