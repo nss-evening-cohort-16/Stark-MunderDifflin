64c3e1f7b4360b6ec82efdd3f79e90c71bb2f545
Daniel Sitarek <deltasierra777@gmail.com>
2022-04-09 13:09:10 -0500

    added controllers/repos

--- Stark-MunderDifflin/Controllers/CustomerController.cs
+++ Stark-MunderDifflin/Controllers/CustomerController.cs
@@ -6,35 +6,35 @@ namespace Stark_MunderDifflin.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PaperController : ControllerBase
+    public class CustomerController : ControllerBase
     {
-        // GET: api/<PaperController>
+        // GET: api/<CustomerController>
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/<PaperController>/5
+        // GET api/<CustomerController>/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/<PaperController>
+        // POST api/<CustomerController>
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/<PaperController>/5
+        // PUT api/<CustomerController>/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/<PaperController>/5
+        // DELETE api/<CustomerController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
