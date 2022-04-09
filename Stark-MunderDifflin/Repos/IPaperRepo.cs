329b162afcbc030f3d2c4ad8c6256210b5fd5320
Daniel Sitarek <deltasierra777@gmail.com>
2022-04-09 13:54:20 -0500

    added PaperRepo with interface, added PaperRepo to PaperController, and created Paper getAll method and endpoint

---
+++ Stark-MunderDifflin/Repos/IPaperRepo.cs
@@ -0,0 +1,10 @@
+﻿using Stark_MunderDifflin.Models;
+
+namespace Stark_MunderDifflin.Repos
+{
+    public interface IPaperRepo
+    {
+        List<Paper> getAll();
+        Paper getById(int id);
+    }
+}
