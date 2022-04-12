84ed3d66b42f3486f857314352644fc5fa8bcaf5
Daniel Sitarek <deltasierra777@gmail.com>
2022-04-11 21:48:51 -0500

    working on orderItem Delete. paper getById complete

---
+++ Stark-MunderDifflin/Repos/IOrderItemRepo.cs
@@ -0,0 +1,9 @@
+﻿using Stark_MunderDifflin.Models;
+
+namespace Stark_MunderDifflin.Repos
+{
+    public interface IOrderItemRepo
+    {
+        public void DeleteOrderItem(int orderId, int paperId);
+    }
+}
