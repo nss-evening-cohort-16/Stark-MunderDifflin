d30708e907dbc7ed43d8071b16fc047a7c19e3db
DerekMalone <laxmalone@gmail.com>
2022-04-12 18:21:28 -0500

    customer GET requests completed.

--- Stark-MunderDifflin/Repos/CustomerRepo.cs
+++ Stark-MunderDifflin/Repos/CustomerRepo.cs
@@ -21,7 +21,37 @@ namespace Stark_MunderDifflin.Repos
         }
         public Customer getCustomerByEmail(string email)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                                        SELECT * FROM Customer
+                                        WHERE Email = @email
+                                        ";
+                    cmd.Parameters.AddWithValue("@email", email);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        Customer customer = new Customer
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            Email = reader.GetString(reader.GetOrdinal("Email")),
+                            UID = reader.GetString(reader.GetOrdinal("UID")),
+                        };
+                        return customer;
+                    }
+                    else
+                    {
+                        reader.Close();
+                        return null;
+                    }
+                }
+            }
         }
 
         public Customer getCustomerByUID(string uid)
