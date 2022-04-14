b3b03da089b1795789a261447ca089394d55478e
Daniel Sitarek <deltasierra777@gmail.com>
2022-04-13 19:49:39 -0500

    added route to update quantity on orderItem

--- Stark-MunderDifflin/Models/Paper.cs
+++ Stark-MunderDifflin/Models/Paper.cs
@@ -3,8 +3,8 @@
     public class Paper
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Color { get; set; }
+        public string? Name { get; set; }
+        public string? Color { get; set; }
         public int Width { get; set; }
         public int Length { get; set; }
         public int Weight {get; set; }
