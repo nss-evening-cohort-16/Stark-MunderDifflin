d8b8f18a977d23ce89dab08f309bae84420c0efb
Messi_G <negash4@gmail.com>
2022-04-19 20:05:07 -0500

    paper detail page setup

---
+++ stark-munderdifflin-client/src/components/PaperDetail.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+
+const PaperDetails = () => {
+  return (
+    <div>HomeComponent</div>
+  )
+}
+
+// HomeComponent.propTypes = {}
+
+export default PaperDetails
