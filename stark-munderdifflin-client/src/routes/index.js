54d6d485941e9af714ad57f725a66c580e23d8af
Daniel Sitarek <deltasierra777@gmail.com>
2022-04-19 20:44:07 -0500

    updated skeleton

--- stark-munderdifflin-client/src/routes/index.js
+++ stark-munderdifflin-client/src/routes/index.js
@@ -1,34 +1,14 @@
-<<<<<<< HEAD
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import { Home, Cart } from '../views/index'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Home, Cart } from '../views/index';
 
 export default function AppRoutes() {
   return (
     <>
-        <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/Cart" element={<Cart />}/>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Cart" element={<Cart />} />
+      </Routes>
     </>
-  )
-}
-=======
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Home from '../views/Home';
-// import Details from '../views/Details';
-// import Cart from '../views/Cart';
-
-export default function Routes() {
-  return (
-    <div>
-      <Switch>
-        <Route exact path={['/', '/home']} component={Home} />
-        {/* <Route exact path="/cart" component={Cart} />
-        <Route exact path="/details" component={Details} /> */}
-      </Switch>
-    </div>
   );
 }
->>>>>>> main
