6d5530afccac01c6f9a43ebeaa2462660e932146
Messi_G <negash4@gmail.com>
2022-04-19 20:05:07 -0500

    paper detail page setup

---
+++ stark-munderdifflin-client/src/views/PaperView.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+
+const PaperView = () => {
+  return (
+    <div>Paper Detail Page</div>
+  )
+}
+
+// Home.propTypes = {}
+
+export default PaperView
