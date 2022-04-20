622ea793f04c882e4e22dbf8947e186e1748acf9
Harika Suvak <hcodes11@gmail.com>
2022-04-19 20:14:25 -0500

    nav

---
+++ stark-munderdifflin-client/src/components/AppNavbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function AppNavbar() {
+  return (
+    <div className="text-center mb-3">
+      <>
+        <Link className="nav-link active" to="/home">
+          Home
+        </Link>
+        <Link className="nav-link active" to="/cart">
+          Cart
+        </Link>
+        <Link className="nav-link active" to="/details">
+          Details
+        </Link>
+      </>
+    </div>
+  );
+}
