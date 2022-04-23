009057e426aae355b6fa2b8cea853b5c5544ba85
Daniel Sitarek <deltasierra777@gmail.com>
2022-04-23 12:29:40 -0500

    saving to merge

--- stark-munderdifflin-client/src/components/CartItem.js
+++ stark-munderdifflin-client/src/components/CartItem.js
@@ -1,22 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import getPaperItemsByOrder from '../data/cartData';
 
 export default function CartItem({ item }) {
-  const [cartItems, setCartItems] = useState([]);
-
-  useEffect(() => {
-    getPaperItemsByOrder(1).then((items) => {
-      setCartItems(items);
-      console.log(items);
-    });
-  }, []);
-
   return (
     <div>
       <table className='cart-table'>
         <thead>
           <tr className='cart-table-row'>
-            <td className='cart-table-cell'></td>
+            <td className='cart-table-cell-h-img'></td>
             <td className='cart-table-cell'>Name</td>
             <td className='cart-table-cell'>Quantity</td>
             <td className='cart-table-cell'>Price</td>
@@ -24,12 +14,12 @@ export default function CartItem({ item }) {
         </thead>
         <tbody>
           <tr className='cart-table-row'>
-            <td className='cart-table-cell'>
-              <img src={item.ImageUrl} alt='paper'></img>
+            <td className='cart-table-cell-r-img'>
+              <img src={item.imageURL} alt='paper'></img>
             </td>
-            <td className='cart-table-cell'>{item.Name}</td>
-            <td className='cart-table-cell'>{item.Quantity}</td>
-            <td className='cart-table-cell'>{item.Price}</td>
+            <td className='cart-table-cell'>{item.name}</td>
+            <td className='cart-table-cell'>{item.quantity}</td>
+            <td className='cart-table-cell'>{item.price}</td>
           </tr>
         </tbody>
       </table>
--- stark-munderdifflin-client/src/views/Cart.js
+++ stark-munderdifflin-client/src/views/Cart.js
@@ -1,36 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CartItem from '../components/CartItem';
+import getPaperItemsByOrder from '../data/cartData';
 
 export default function Cart() {
-  const [cartItems] = useState([
-    {
-      Quantity: 3,
-      Id: 1,
-      Name: 'Arches 88 Sild Screen Paper',
-      Color: 'White',
-      Width: 30,
-      Length: 22,
-      Weight: 140,
-      Price: 2.99,
-      ImageUrl: 'https://i.imgur.com/3aucv1x.jpg',
-    },
-    {
-      Quantity: 2,
-      Id: 3,
-      Name: 'Legion Somerset Printmaking Paper',
-      Color: 'Antique',
-      Width: 44,
-      Length: 30,
-      Weight: 280,
-      Price: 3.99,
-      ImageUrl: 'https://i.imgur.com/jKFsoJs.jpg',
-    },
-  ]);
+  const [cartItems, setCartItems] = useState([]);
+
+  useEffect(() => {
+    getPaperItemsByOrder(1).then((items) => {
+      setCartItems(items);
+      console.log(items);
+    });
+  }, []);
+
   return (
     <div className='cart-container'>
       <div className='cart-items-container'>
         {cartItems.map((item) => (
-          <CartItem key={item.Id} item={item} />
+          <CartItem key={item.id} item={item} />
         ))}
       </div>
       <button className='btn btn-success cart-submit'>Submit</button>
