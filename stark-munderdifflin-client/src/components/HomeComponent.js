61927fd110d1f324a40a38f8c8e694263357cc1f
Daniel Sitarek <deltasierra777@gmail.com>
2022-04-23 14:04:13 -0500

    saving to merge

--- stark-munderdifflin-client/src/components/HomeComponent.js
+++ stark-munderdifflin-client/src/components/HomeComponent.js
@@ -10,28 +10,25 @@ import {
 
 export default function HomeComponent({ paper }) {
   return (
-    <div className ="home-page">
-<Card className="paper-card">
-        <CardTitle className="paper-name">{paper.name}</CardTitle>
-        <CardImg 
-          alt="paper image"
-          className="paper-image"
+    <div className='home-page'>
+      <Card className='paper-card'>
+        <CardTitle className='paper-name'>{paper.name}</CardTitle>
+        <CardImg
+          src={paper.imageURL}
+          alt='paper image'
+          className='paper-image'
         />
         <CardBody>
-          <CardSubtitle className="paper-color">{paper.color}        
-          </CardSubtitle>
-          <div className="card-btn-container">          
-            <Button
-                className="add-to-cart"
-                type="button"
-              >
-                <i className="btn btn-primary" />
-              </Button>        
+          <CardSubtitle className='paper-color'>{paper.color}</CardSubtitle>
+          <div className='card-btn-container'>
+            <Button className='add-to-cart' type='button'>
+              <i className='btn btn-primary' />
+            </Button>
           </div>
         </CardBody>
-      </Card>   
+      </Card>
     </div>
-  )
+  );
 }
 
 // Home.propTypes = {}
