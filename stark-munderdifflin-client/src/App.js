04aad88b145cb892d63d9dc671f88b644fea9001
Daniel Sitarek <deltasierra777@gmail.com>
2022-04-27 19:44:06 -0500

    added icon for crat update qty

--- stark-munderdifflin-client/src/App.js
+++ stark-munderdifflin-client/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Routes from './routes/index';
 import AppNavbar from './components/AppNavbar';
+import databaseConfig from './data/auth/apiKeys';
 
 function App() {
   return (
