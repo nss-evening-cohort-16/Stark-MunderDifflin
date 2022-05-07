366daea3328f4b57182c5e05064f6cfdbecf1d12
DerekMalone <laxmalone@gmail.com>
2022-05-06 19:17:32 -0500

    User logged in synced with edit button showing

--- stark-munderdifflin-client/src/components/HomeComponent.js
+++ stark-munderdifflin-client/src/components/HomeComponent.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import {
   Card,
   CardTitle,
@@ -7,41 +7,44 @@ import {
   Button,
   CardSubtitle,
   CardImg,
-} from 'reactstrap';
+} from "reactstrap";
 
-export default function HomeComponent({ paper }) {
+export default function HomeComponent({ paper, user }) {
   const navigate = useNavigate();
 
   return (
-
-    <div className ="home-container">
-<Card className="paper-card">
-        <CardTitle className="paper-name">{paper.name}</CardTitle>
-        <CardImg 
-          alt="paper image"
-          className="paper-image"
+    <div className='home-container'>
+      <Card className='paper-card'>
+        <CardTitle className='paper-name'>{paper.name}</CardTitle>
+        <CardImg
+          alt='paper image'
+          className='paper-image'
           src={paper.imageURL}
           onClick={() => navigate(`/PaperDetail/${paper.id}`)}
         />
         <CardBody>
-          <CardSubtitle className='paper-color'>{paper.color}</CardSubtitle> 
-          <div className="paper-btn-container"> 
-          
-          <Button className='add-to-cart' type='button'>
-            Add to Cart
-          </Button>
-          <Button
-                className="edit-paper"
-                onClick={() => navigate(`/Edit/${paper.id}`)}
-              >
-            Edit
-              </Button>  
+          <CardSubtitle className='paper-color'>{paper.color}</CardSubtitle>
+          <div className='paper-btn-container'>
+            <Button className='add-to-cart' type='button'>
+              Add to Cart
+            </Button>
+            {!user ? (
+              ''
+            ) : (
+              <>
+                <Button
+                  className='edit-paper'
+                  onClick={() => navigate(`/Edit/${paper.id}`)}
+                >
+                  Edit
+                </Button>
+              </>
+            )}
           </div>
         </CardBody>
       </Card>
     </div>
-
-  )
+  );
 }
 
 // Home.propTypes = {}
